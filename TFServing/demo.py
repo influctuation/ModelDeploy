fa11cd415dee9e22d98a974a73c5876eb6dd7b65
廖望 <3428232535@qq.com>
2023-06-16 01:18:16 +0800

    add tf serving demo

---
+++ TFServing/demo.py
@@ -0,0 +1,23 @@
+import json,cv2, requests
+import numpy as np
+
+im = cv2.imread('test.png')
+blob = cv2.dnn.blobFromImage(im, 1/ 255.0, (640, 640), (0,0,0), swapRB=True, crop=False)
+im = cv2.resize(im, (640, 640)).reshape(1,640,640,3)
+data = json.dumps({"instances": blob.tolist()})
+data1 = json.dumps({"instances": im.tolist()})
+headers = {"content-type": "application/json"}
+response = requests.post("http://localhost:8501/v1/models/carafe/versions/1:predict", data=data, headers=headers)
+content = response.content.decode('utf-8')
+results = np.array(json.loads(content)['predictions'][0]["output0"])
+print(f"results.shape: {results.shape}")
+response = requests.post("http://localhost:8501/v1/models/carafe/labels/canary:predict", data=data, headers=headers)
+content = response.content.decode('utf-8')
+results = np.array(json.loads(content)['predictions'][0]["output0"])
+print(f"results.shape: {results.shape}")
+response = requests.post("http://localhost:8501/v1/models/bw/versions/22:predict", data=data1, headers=headers)
+content = response.content.decode('utf-8')
+results = np.array(json.loads(content)['predictions'][0])
+print(f"results.shape: {results.shape}")
+# docker cli
+# docker run -it --rm --name yolo-tfserving -p 8501:8501 --gpus all -v "$(pwd)/models/carafe:/models/carafe" -e MODEL_NAME=carafe tensorflow/serving:latest-gpu
